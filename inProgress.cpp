525a01e47fd3e61c525f0c22eff9b00fb8db1fe7
Arman-Ghazaryan <91345263+Arman-Ghazaryan@users.noreply.github.com>
2022-02-22 23:00:20 +0400

    Update inProgress.cpp

--- inProgress.cpp
+++ inProgress.cpp
@@ -4,19 +4,73 @@
 using namespace std;
 
 template<typename T>
-class List_iterator;
-
-template<typename T>
 class list
 {
 public:
+	template<typename T> struct Node;
+
+	template <typename T> struct List_iterator;
 	typedef List_iterator<T> Iterator;
+
+	template <typename T>
+	struct List_iterator
+	{
+	public:
+
+		List_iterator(Node<T>* source_iterator) : current_iterator(source_iterator) {};
+		List_iterator() : current_iterator(0) { }
+
+		T& operator*()
+		{
+			return current_iterator->data;
+		}
+		Iterator& operator=(List_iterator<T>* source_it)
+		{
+			this->current_iterator = (Node<T>*)source_it;
+			return *this;
+		}
+		bool operator==(Node<T>* source_it) const
+		{
+			return current_iterator == source_it;
+		}
+		bool operator!=(Node<T>* source_it) const
+		{
+			return current_iterator != source_it;
+		}
+		Iterator& operator++()
+		{
+			current_iterator = current_iterator->pNext;
+			return *this;
+		}
+		Iterator operator++(int)
+		{
+			Iterator it = *this;
+			++* this;
+			return it;
+		}
+		/*Iterator& operator--()
+		{
+			Iterator it = begin();
+			for (; it != current_iterator; it = it->pNext)
+				if (it->pNext == current_iterator)return Iterator&(it);
+		}
+		Iterator operator--(int)
+		{
+			Iterator it = *this;
+			--*this;
+			return it;
+		}*/
+
+	private:
+		Node<T>* current_iterator;
+	};
 
 	list();
 	~list();
 
 	//Adding elements//
 	void push_back(T data);
+	void push_back(vector<T> data);
 	void push_front(T data);
 	void insert(T data, int pos);
 	void insert(T data, int count, int pos);
@@ -31,8 +85,8 @@ public:
 	////
 
 	//Geting info//
-	Iterator* begin() const;
-	Iterator* end() const;
+	Iterator* begin() { return (List_iterator<T>*)Begin; }
+	Iterator* end() { return (List_iterator<T>*)End; }
 	bool empty();
 	T front();
 	T back();
@@ -48,8 +102,9 @@ private:
 	};
 
 	size_t size;
-	Node<T> *End;
-	Node<T> *Begin;
+	Node<T>* End;
+	Node<T>* Begin;
+
 };
 
 template<typename T>
@@ -65,32 +120,6 @@ list<T>::~list()
 {
 	clear();
 }
-
-
-template <typename T> 
-class List_iterator
-{
-public:
-	typedef List_iterator<T> Iterator;
-
-	List_iterator(List_iterator<T>* source_iterator);
-	List_iterator() : current_iterator(0) { }
-
-	T& operator*();  // dereferencing operator
-	Iterator& operator=(const Iterator& rhs) ;
-	bool operator==(const Iterator& rhs) const;
-	bool operator!=(const Iterator& rhs) const;
-	Iterator& operator++();
-	Iterator operator++(int);
-	Iterator& operator--();
-	Iterator operator--(int);
-
-private:
-	List_iterator<T>* current_iterator;
-
-	friend class list<T>;
-};
-
 
 template<typename T>
 void list<T>::push_back(T data)
@@ -112,6 +141,31 @@ void list<T>::push_back(T data)
 	size++;
 }
 
+template <typename T>
+void list<T>::push_back(vector<T> data)
+{
+	Node<T>* temp;
+
+	for (int i = 0; i < data.size(); i++)
+	{
+		temp = new Node<T>;
+		temp->data = data[i];
+		temp->pNext = NULL;
+		if (Begin == NULL)
+		{
+			Begin = temp;
+			End = temp;
+			temp = NULL;
+		}
+		else
+		{
+			End->pNext = temp;
+			End = temp;
+		}
+	}
+	size += data.size();
+}
+
 template<typename T>
 void list<T>::push_front(T data)
 {
@@ -125,7 +179,7 @@ void list<T>::push_front(T data)
 template<typename T>
 void list<T>::pop_front()
 {
-	Node<T> *temp = Begin;
+	Node<T>* temp = Begin;
 	Begin = Begin->pNext;
 	delete temp;
 	size--;
@@ -166,7 +220,7 @@ void list<T>::insert(T data, int count, int pos)
 {
 	if (pos == 0)
 	{
-		for (int i = 0; i < count;i++)
+		for (int i = 0; i < count; i++)
 		{
 			push_front(data);
 		}
@@ -182,22 +236,20 @@ void list<T>::insert(T data, int count, int pos)
 	{
 		Node<T>* previous = this->Begin;
 		Node<T>* temp = new Node<T>;
-		Node<T>* temp1 = new Node<T>;
+		vector<T> sData(count, data);
 
 		for (int i = 0; i < pos - 1; i++)
 		{
 			previous = previous->pNext;
 		}
 
-		temp1 = previous->pNext->pNext;
 		temp = End;
-		End = previous->pNext;
+		End = previous;
+		previous = previous->pNext;
 
-		for (int i = 0; i < count; i++)
-		{
-			push_back(data);
-		}
-		End->pNext = temp1;
+		push_back(sData);
+
+		End->pNext = previous;
 		End = temp;
 	}
 }
@@ -216,29 +268,26 @@ void list<T>::insert(vector<int> vec, int pos)
 	{
 		for (int i = 0; i < vec.size(); i++)
 		{
-			push_back(vec[i]);
+			push_back(vec);
 		}
 	}
 	else
 	{
 		Node<T>* previous = this->Begin;
 		Node<T>* temp = new Node<T>;
-		Node<T>* temp1 = new Node<T>;
 
 		for (int i = 0; i < pos - 1; i++)
 		{
 			previous = previous->pNext;
 		}
 
-		temp1 = previous->pNext->pNext;
 		temp = End;
-		End = previous->pNext;
+		End = previous;
+		previous = previous->pNext;
 
-		for (int i = 0; i < vec.size(); i++)
-		{
-			push_back(vec[i]);
-		}
-		End->pNext = temp1;
+		push_back(vec);
+
+		End->pNext = previous;
 		End = temp;
 	}
 }
@@ -246,7 +295,7 @@ void list<T>::insert(vector<int> vec, int pos)
 template<typename T>
 void list<T>::clear()
 {
-	while (size) 
+	while (size)
 	{
 		pop_front();
 	}
@@ -274,20 +323,6 @@ void list<T>::removeAt(int pos)
 	size--;
 }
 
-
-
-template<typename T>
-typename list<T>::Iterator* list<T>::begin() const
-{
-	return Iterator(Begin);
-}
-
-template<typename T>
-typename list<T>::Iterator* list<T>::end() const
-{
-	return Iterator(End);
-}
-
 template<typename T>
 bool list<T>::empty()
 {
@@ -310,7 +345,6 @@ T list<T>::back()
 	return End->data;
 }
 
-
 template<typename T>
 T& list<T>::operator[](const int index)
 {
@@ -327,6 +361,7 @@ T& list<T>::operator[](const int index)
 	}
 }
 
+
 int main()
 {
 	list<int> lst;
@@ -334,7 +369,10 @@ int main()
 	lst.push_back(11);
 	lst.push_back(12);
 	lst.push_back(13);
-	it = lst.begin();
+	it = (lst.begin());
+	it++;
+	it++;
+	//it--;
 	lst.push_back(14);
 	lst.push_back(15);
 	lst.insert(17, 3);
@@ -342,12 +380,6 @@ int main()
 	lst.pop_front();
 	lst.removeAt(3);
 	lst.insert(3, 3, 2);
-//	lst.insert({ 0,1,2 }, 5);
+	lst.insert({ 0,1,2 }, 5);
 
-}
-
-template<typename T>
-List_iterator<T>::List_iterator(List_iterator<T>* source_iterator)
-{
-	current_iterator = source_iterator;
 }
